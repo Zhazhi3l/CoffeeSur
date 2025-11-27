b6e8a7c92b9eab7724df6992385937fadb157f0b
CarlosSantiagoChipahua <s23120236@alumnos.itsur.edu.mx>
2025-11-27 13:43:55 -0600

    Fill en Auditorias

--- UI/FrmAuditorias.Designer.cs
+++ UI/FrmAuditorias.Designer.cs
@@ -20,117 +20,186 @@
 			base.Dispose(disposing);
 		}
 
-        #region Windows Form Designer generated code
+		#region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent()
-        {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmAuditorias));
-            label1 = new Label();
-            btnUsuarios = new Button();
-            btnProductos = new Button();
-            btnVentas = new Button();
-            dgvAuditorias = new DataGridView();
-            ((System.ComponentModel.ISupportInitialize)dgvAuditorias).BeginInit();
-            SuspendLayout();
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Font = new Font("Segoe UI", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label1.Location = new Point(600, 9);
-            label1.Margin = new Padding(4, 0, 4, 0);
-            label1.Name = "label1";
-            label1.Size = new Size(151, 37);
-            label1.TabIndex = 0;
-            label1.Text = "Auditorías";
-            // 
-            // btnUsuarios
-            // 
-            btnUsuarios.BackColor = Color.BurlyWood;
-            btnUsuarios.Font = new Font("Footlight MT Light", 14.25F);
-            btnUsuarios.Image = (Image)resources.GetObject("btnUsuarios.Image");
-            btnUsuarios.ImageAlign = ContentAlignment.MiddleLeft;
-            btnUsuarios.Location = new Point(93, 103);
-            btnUsuarios.Margin = new Padding(4, 3, 4, 3);
-            btnUsuarios.Name = "btnUsuarios";
-            btnUsuarios.Size = new Size(241, 67);
-            btnUsuarios.TabIndex = 1;
-            btnUsuarios.Text = "Usuarios";
-            btnUsuarios.TextAlign = ContentAlignment.MiddleRight;
-            btnUsuarios.UseVisualStyleBackColor = false;
-            btnUsuarios.Click += btnUsuarios_Click;
-            // 
-            // btnProductos
-            // 
-            btnProductos.BackColor = Color.BurlyWood;
-            btnProductos.Image = (Image)resources.GetObject("btnProductos.Image");
-            btnProductos.ImageAlign = ContentAlignment.MiddleLeft;
-            btnProductos.Location = new Point(593, 103);
-            btnProductos.Margin = new Padding(4, 3, 4, 3);
-            btnProductos.Name = "btnProductos";
-            btnProductos.Size = new Size(241, 67);
-            btnProductos.TabIndex = 2;
-            btnProductos.Text = "Productos";
-            btnProductos.TextAlign = ContentAlignment.MiddleRight;
-            btnProductos.UseVisualStyleBackColor = false;
-            btnProductos.Click += btnProductos_Click;
-            // 
-            // btnVentas
-            // 
-            btnVentas.BackColor = Color.BurlyWood;
-            btnVentas.Image = (Image)resources.GetObject("btnVentas.Image");
-            btnVentas.ImageAlign = ContentAlignment.MiddleLeft;
-            btnVentas.Location = new Point(1107, 103);
-            btnVentas.Margin = new Padding(4, 3, 4, 3);
-            btnVentas.Name = "btnVentas";
-            btnVentas.Size = new Size(241, 67);
-            btnVentas.TabIndex = 3;
-            btnVentas.Text = "Ventas";
-            btnVentas.TextAlign = ContentAlignment.MiddleRight;
-            btnVentas.UseVisualStyleBackColor = false;
-            btnVentas.Click += btnVentas_Click;
-            // 
-            // dgvAuditorias
-            // 
-            dgvAuditorias.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
-            dgvAuditorias.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dgvAuditorias.Location = new Point(14, 213);
-            dgvAuditorias.Margin = new Padding(4, 3, 4, 3);
-            dgvAuditorias.Name = "dgvAuditorias";
-            dgvAuditorias.RowHeadersWidth = 51;
-            dgvAuditorias.Size = new Size(1381, 395);
-            dgvAuditorias.TabIndex = 4;
-            // 
-            // FrmAuditorias
-            // 
-            AutoScaleDimensions = new SizeF(10F, 20F);
-            AutoScaleMode = AutoScaleMode.Font;
-            BackColor = Color.Wheat;
-            ClientSize = new Size(1370, 675);
-            Controls.Add(dgvAuditorias);
-            Controls.Add(btnVentas);
-            Controls.Add(btnProductos);
-            Controls.Add(btnUsuarios);
-            Controls.Add(label1);
-            Font = new Font("Footlight MT Light", 14.25F);
-            Margin = new Padding(4, 3, 4, 3);
-            Name = "FrmAuditorias";
-            Text = "FrmAuditorias";
-            ((System.ComponentModel.ISupportInitialize)dgvAuditorias).EndInit();
-            ResumeLayout(false);
-            PerformLayout();
-        }
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmAuditorias));
+			label1 = new Label();
+			btnUsuarios = new Button();
+			btnProductos = new Button();
+			btnVentas = new Button();
+			dgvAuditorias = new DataGridView();
+			tableLayoutPanel1 = new TableLayoutPanel();
+			tableLayoutPanel3 = new TableLayoutPanel();
+			tableLayoutPanel2 = new TableLayoutPanel();
+			((System.ComponentModel.ISupportInitialize)dgvAuditorias).BeginInit();
+			tableLayoutPanel1.SuspendLayout();
+			tableLayoutPanel3.SuspendLayout();
+			tableLayoutPanel2.SuspendLayout();
+			SuspendLayout();
+			// 
+			// label1
+			// 
+			label1.Dock = DockStyle.Fill;
+			label1.Font = new Font("Segoe UI", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, 0);
+			label1.Location = new Point(132, 6);
+			label1.Margin = new Padding(4, 0, 4, 0);
+			label1.Name = "label1";
+			label1.Size = new Size(1019, 51);
+			label1.TabIndex = 0;
+			label1.Text = "Auditorías";
+			label1.TextAlign = ContentAlignment.MiddleCenter;
+			// 
+			// btnUsuarios
+			// 
+			btnUsuarios.BackColor = Color.BurlyWood;
+			btnUsuarios.Dock = DockStyle.Fill;
+			btnUsuarios.Font = new Font("Footlight MT Light", 14.25F);
+			btnUsuarios.Image = (Image)resources.GetObject("btnUsuarios.Image");
+			btnUsuarios.ImageAlign = ContentAlignment.MiddleLeft;
+			btnUsuarios.Location = new Point(68, 3);
+			btnUsuarios.Margin = new Padding(4, 3, 4, 3);
+			btnUsuarios.Name = "btnUsuarios";
+			btnUsuarios.Size = new Size(377, 58);
+			btnUsuarios.TabIndex = 1;
+			btnUsuarios.Text = "Usuarios";
+			btnUsuarios.TextAlign = ContentAlignment.MiddleRight;
+			btnUsuarios.UseVisualStyleBackColor = false;
+			btnUsuarios.Click += btnUsuarios_Click;
+			// 
+			// btnProductos
+			// 
+			btnProductos.BackColor = Color.BurlyWood;
+			btnProductos.Dock = DockStyle.Fill;
+			btnProductos.Image = (Image)resources.GetObject("btnProductos.Image");
+			btnProductos.ImageAlign = ContentAlignment.MiddleLeft;
+			btnProductos.Location = new Point(453, 3);
+			btnProductos.Margin = new Padding(4, 3, 4, 3);
+			btnProductos.Name = "btnProductos";
+			btnProductos.Size = new Size(377, 58);
+			btnProductos.TabIndex = 2;
+			btnProductos.Text = "Productos";
+			btnProductos.TextAlign = ContentAlignment.MiddleRight;
+			btnProductos.UseVisualStyleBackColor = false;
+			btnProductos.Click += btnProductos_Click;
+			// 
+			// btnVentas
+			// 
+			btnVentas.BackColor = Color.BurlyWood;
+			btnVentas.Dock = DockStyle.Fill;
+			btnVentas.Image = (Image)resources.GetObject("btnVentas.Image");
+			btnVentas.ImageAlign = ContentAlignment.MiddleLeft;
+			btnVentas.Location = new Point(838, 3);
+			btnVentas.Margin = new Padding(4, 3, 4, 3);
+			btnVentas.Name = "btnVentas";
+			btnVentas.Size = new Size(377, 58);
+			btnVentas.TabIndex = 3;
+			btnVentas.Text = "Ventas";
+			btnVentas.TextAlign = ContentAlignment.MiddleRight;
+			btnVentas.UseVisualStyleBackColor = false;
+			btnVentas.Click += btnVentas_Click;
+			// 
+			// dgvAuditorias
+			// 
+			dgvAuditorias.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+			dgvAuditorias.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			dgvAuditorias.Dock = DockStyle.Fill;
+			dgvAuditorias.Location = new Point(75, 178);
+			dgvAuditorias.Margin = new Padding(4, 3, 4, 3);
+			dgvAuditorias.Name = "dgvAuditorias";
+			dgvAuditorias.RowHeadersWidth = 51;
+			dgvAuditorias.Size = new Size(1282, 488);
+			dgvAuditorias.TabIndex = 4;
+			// 
+			// tableLayoutPanel1
+			// 
+			tableLayoutPanel1.ColumnCount = 3;
+			tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 5F));
+			tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 90F));
+			tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 5F));
+			tableLayoutPanel1.Controls.Add(tableLayoutPanel3, 1, 2);
+			tableLayoutPanel1.Controls.Add(tableLayoutPanel2, 1, 1);
+			tableLayoutPanel1.Controls.Add(dgvAuditorias, 1, 3);
+			tableLayoutPanel1.Dock = DockStyle.Fill;
+			tableLayoutPanel1.Location = new Point(0, 0);
+			tableLayoutPanel1.Name = "tableLayoutPanel1";
+			tableLayoutPanel1.RowCount = 5;
+			tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 5F));
+			tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 10F));
+			tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 10F));
+			tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 70F));
+			tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 5F));
+			tableLayoutPanel1.Size = new Size(1434, 707);
+			tableLayoutPanel1.TabIndex = 5;
+			// 
+			// tableLayoutPanel3
+			// 
+			tableLayoutPanel3.ColumnCount = 5;
+			tableLayoutPanel3.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 5F));
+			tableLayoutPanel3.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 30F));
+			tableLayoutPanel3.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 30F));
+			tableLayoutPanel3.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 30F));
+			tableLayoutPanel3.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 5F));
+			tableLayoutPanel3.Controls.Add(btnUsuarios, 1, 0);
+			tableLayoutPanel3.Controls.Add(btnVentas, 3, 0);
+			tableLayoutPanel3.Controls.Add(btnProductos, 2, 0);
+			tableLayoutPanel3.Dock = DockStyle.Fill;
+			tableLayoutPanel3.Location = new Point(74, 108);
+			tableLayoutPanel3.Name = "tableLayoutPanel3";
+			tableLayoutPanel3.RowCount = 1;
+			tableLayoutPanel3.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+			tableLayoutPanel3.Size = new Size(1284, 64);
+			tableLayoutPanel3.TabIndex = 6;
+			// 
+			// tableLayoutPanel2
+			// 
+			tableLayoutPanel2.ColumnCount = 3;
+			tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 10F));
+			tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 80F));
+			tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 10F));
+			tableLayoutPanel2.Controls.Add(label1, 1, 1);
+			tableLayoutPanel2.Dock = DockStyle.Fill;
+			tableLayoutPanel2.Location = new Point(74, 38);
+			tableLayoutPanel2.Name = "tableLayoutPanel2";
+			tableLayoutPanel2.RowCount = 3;
+			tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 10F));
+			tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 80F));
+			tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 10F));
+			tableLayoutPanel2.Size = new Size(1284, 64);
+			tableLayoutPanel2.TabIndex = 6;
+			// 
+			// FrmAuditorias
+			// 
+			AutoScaleDimensions = new SizeF(13F, 25F);
+			AutoScaleMode = AutoScaleMode.Font;
+			BackColor = Color.Wheat;
+			ClientSize = new Size(1434, 707);
+			Controls.Add(tableLayoutPanel1);
+			Font = new Font("Footlight MT Light", 14.25F);
+			Margin = new Padding(4, 3, 4, 3);
+			Name = "FrmAuditorias";
+			Text = "FrmAuditorias";
+			((System.ComponentModel.ISupportInitialize)dgvAuditorias).EndInit();
+			tableLayoutPanel1.ResumeLayout(false);
+			tableLayoutPanel3.ResumeLayout(false);
+			tableLayoutPanel2.ResumeLayout(false);
+			ResumeLayout(false);
+		}
 
-        #endregion
+		#endregion
 
-        private Label label1;
+		private Label label1;
 		private Button btnUsuarios;
 		private Button btnProductos;
 		private Button btnVentas;
 		private DataGridView dgvAuditorias;
+		private TableLayoutPanel tableLayoutPanel1;
+		private TableLayoutPanel tableLayoutPanel3;
+		private TableLayoutPanel tableLayoutPanel2;
 	}
 }
