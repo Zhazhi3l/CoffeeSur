258461a7dab10e1cf11dbc8ccc7542080ebd97ad
CarlosSantiagoChipahua <s23120236@alumnos.itsur.edu.mx>
2025-11-28 21:03:28 -0600

    Secion Usuario

---
+++ Modelos/SesionUsuario.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeSur.Modelos
+{
+	public class SesionUsuario
+	{
+		public int IdUsuario { get; set; }
+		public string Nombre { get; set; }
+		public string Username { get; set; }
+		public string Rol { get; set; }
+		public bool EstaAutenticado { get; set; }
+
+		public SesionUsuario()
+		{
+			EstaAutenticado = false;
+		}
+
+		public void IniciarSesion(Usuario usuario)
+		{
+			IdUsuario = usuario.IdUsuario;
+			Nombre = usuario.Nombre;
+			Username = usuario.Username;
+			Rol = usuario.Rol;
+			EstaAutenticado = true;
+		}
+
+		public void CerrarSesion()
+		{
+			IdUsuario = 0;
+			Nombre = string.Empty;
+			Username = string.Empty;
+			Rol = string.Empty;
+			EstaAutenticado = false;
+		}
+	}
+}
---
+++ Repositorios/SesionUsuarioRepository.cs
@@ -0,0 +1,48 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeSur.Repositorios
+{
+	public class SesionUsuarioRepository 
+	{
+		private readonly ConexionBD _conexion = new ConexionBD();
+
+		/// <summary>
+		/// Establece el usuario de la aplicación en la sesión MySQL para los triggers de auditoría
+		/// </summary>
+		/// <param name="username">Username del usuario autenticado</param>
+		public void EstablecerUsuarioAplicacion(string username)
+		{
+			using (MySqlConnection conex = _conexion.GetConexion())
+			{
+				using (MySqlCommand cmd = new MySqlCommand("sp_SetAppUser", conex))
+				{
+					cmd.CommandType = CommandType.StoredProcedure;
+					cmd.Parameters.AddWithValue("@p_Username", username);
+					cmd.ExecuteNonQuery();
+				}
+			}
+		}
+
+		/// <summary>
+		/// Limpia el usuario de la aplicación en la sesión MySQL
+		/// </summary>
+		public void LimpiarUsuarioAplicacion()
+		{
+			using (MySqlConnection conex = _conexion.GetConexion())
+			{
+				using (MySqlCommand cmd = new MySqlCommand("sp_SetAppUser", conex))
+				{
+					cmd.CommandType = CommandType.StoredProcedure;
+					cmd.Parameters.AddWithValue("@p_Username", DBNull.Value);
+					cmd.ExecuteNonQuery();
+				}
+			}
+		}
+	}
+}
